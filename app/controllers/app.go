af2b4157873071abca901a929cf990580127c2ba
richtr <rich.tibbett@gmail.com>
2014-05-06 12:01:32 +0200

    Add simple people finder utility to BaseApp

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -2,6 +2,10 @@ package controllers
 
 import (
 	r "github.com/revel/revel"
+	"github.com/richtr/baseapp/app/models"
+	"github.com/richtr/baseapp/app/routes"
+	"net/url"
+	"strings"
 )
 
 type Application struct {
@@ -19,4 +23,45 @@ func (c Application) About() r.Result {
 
 func (c Application) Contact() r.Result {
 	return c.Render()
+}
+
+func (c Application) Search(query string, page int) r.Result {
+
+	var matchedProfiles []*models.Profile
+
+	if page == 0 {
+		page = 1
+	}
+	nextPage := page + 1
+	size := 50; // results per page
+
+	if(query != "") {
+
+		// Format query value
+		sql_query_value, _ := url.QueryUnescape(query);
+		sql_query_value = strings.Trim(sql_query_value, " @#")
+		sql_query_value = strings.ToLower(sql_query_value)
+
+		sql_query_value_full := "%" + sql_query_value + "%"
+		sql_query_value_front := sql_query_value + "%"
+		sql_query_value_back := "%" + sql_query_value
+
+		sql_query_string := "SELECT * FROM Profile WHERE username LIKE ? OR name LIKE ? ORDER BY CASE WHEN username LIKE ? THEN 1 WHEN name LIKE ? THEN 2 WHEN username LIKE ? THEN 4 WHEN name LIKE ? THEN 5 ELSE 3 END LIMIT ?, ?"
+
+		// Retrieve all profiles loosely matching search term
+		results, err := c.Txn.Select(models.Profile{}, sql_query_string, sql_query_value_full, sql_query_value_full, sql_query_value_front, sql_query_value_front, sql_query_value_back, sql_query_value_back, (page-1)*size, size)
+
+		if err == nil {
+			for _, r := range results {
+				matchedProfiles = append(matchedProfiles, r.(*models.Profile))
+			}
+		}
+
+		if len(matchedProfiles) == 0 && page != 1 {
+			return c.Redirect(routes.Application.Search(query, 1))
+		}
+
+	}
+
+	return c.Render(query, matchedProfiles, page, nextPage)
 }
