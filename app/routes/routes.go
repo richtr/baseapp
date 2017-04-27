686d7a305d627aff386a6a8907d7bfc22a3a8e02
Rich Tibbett <rich.tibbett@gmail.com>
2017-04-27 12:51:43 +0200

    Update GoDeps

--- app/routes/routes.go
+++ app/routes/routes.go
@@ -4,6 +4,32 @@ package routes
 import "github.com/revel/revel"
 
 
+type tGorpController struct {}
+var GorpController tGorpController
+
+
+func (_ tGorpController) Begin(
+		) string {
+	args := make(map[string]string)
+	
+	return revel.MainRouter.Reverse("GorpController.Begin", args).URL
+}
+
+func (_ tGorpController) Commit(
+		) string {
+	args := make(map[string]string)
+	
+	return revel.MainRouter.Reverse("GorpController.Commit", args).URL
+}
+
+func (_ tGorpController) Rollback(
+		) string {
+	args := make(map[string]string)
+	
+	return revel.MainRouter.Reverse("GorpController.Rollback", args).URL
+}
+
+
 type tApplication struct {}
 var Application tApplication
 
@@ -55,32 +81,6 @@ func (_ tApplication) SwitchToMobile(
 }
 
 
-type tGorpController struct {}
-var GorpController tGorpController
-
-
-func (_ tGorpController) Begin(
-		) string {
-	args := make(map[string]string)
-	
-	return revel.MainRouter.Reverse("GorpController.Begin", args).URL
-}
-
-func (_ tGorpController) Commit(
-		) string {
-	args := make(map[string]string)
-	
-	return revel.MainRouter.Reverse("GorpController.Commit", args).URL
-}
-
-func (_ tGorpController) Rollback(
-		) string {
-	args := make(map[string]string)
-	
-	return revel.MainRouter.Reverse("GorpController.Rollback", args).URL
-}
-
-
 type tJobs struct {}
 var Jobs tJobs
 
@@ -90,6 +90,45 @@ func (_ tJobs) Status(
 	args := make(map[string]string)
 	
 	return revel.MainRouter.Reverse("Jobs.Status", args).URL
+}
+
+
+type tTestRunner struct {}
+var TestRunner tTestRunner
+
+
+func (_ tTestRunner) Index(
+		) string {
+	args := make(map[string]string)
+	
+	return revel.MainRouter.Reverse("TestRunner.Index", args).URL
+}
+
+func (_ tTestRunner) Suite(
+		suite string,
+		) string {
+	args := make(map[string]string)
+	
+	revel.Unbind(args, "suite", suite)
+	return revel.MainRouter.Reverse("TestRunner.Suite", args).URL
+}
+
+func (_ tTestRunner) Run(
+		suite string,
+		test string,
+		) string {
+	args := make(map[string]string)
+	
+	revel.Unbind(args, "suite", suite)
+	revel.Unbind(args, "test", test)
+	return revel.MainRouter.Reverse("TestRunner.Run", args).URL
+}
+
+func (_ tTestRunner) List(
+		) string {
+	args := make(map[string]string)
+	
+	return revel.MainRouter.Reverse("TestRunner.List", args).URL
 }
 
 
